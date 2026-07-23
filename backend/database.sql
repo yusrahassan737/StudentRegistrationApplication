4317da42703901c2a273b5825b08b48017b87638
Yusra <yusrahassan737@gmail.com>
2026-07-23 18:56:40 -0400

    updated sql for robustness

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE college;
+CREATE DATABASE IF NOT EXISTS college;
 USE college;
-CREATE TABLE students (
+CREATE TABLE IF NOT EXISTS students (
     studentID INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(50),
     lastName VARCHAR(50),
